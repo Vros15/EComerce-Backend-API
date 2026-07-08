5c6e110b4f01bab0b38aac8c0498a598c9a953bb
Victor Rosario <vros15@yahoo.com>
2026-07-08 15:31:40 -0400

    feat: add GET route for retrieving all customers

--- routes/customersRouter.js
+++ routes/customersRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {createCustomer} = require("../controllers/customerController");
+const {createCustomer, getAllCustomers} = require("../controllers/customerController");
 
 // Route to create a new customer
+//endpoint: POST /api/customers
 router.post("/", createCustomer);
 
+// Route to get all customers
+//endpoint: GET /api/customers
+router.get("/", getAllCustomers);
 
 module.exports = router;
 
