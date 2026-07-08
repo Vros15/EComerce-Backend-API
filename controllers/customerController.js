ba6d515083b5faaea24c44715998ebbd71b396a7
Victor Rosario <vros15@yahoo.com>
2026-07-08 15:31:14 -0400

    feat: add controller to retrieve all customers

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -11,6 +11,16 @@ const createCustomer = async (req, res) => {
     }
 };
 
+const getAllCustomers = async (req, res) => {
+    try {
+        const customers = await Customer.find();
+        res.status(200).json({ customers });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching customers", error });
+    }
+};
+
 module.exports = {
     createCustomer,
+    getAllCustomers,
 };
