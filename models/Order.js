37663e730c6dffefe9903ba8a9ee0be13b02bb1e
Victor Rosario <vros15@yahoo.com>
2026-07-08 15:04:07 -0400

    feat: add Order model with customer, products, and statuswx

---
+++ models/Order.js
@@ -0,0 +1,42 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+//create a schema for the order
+const orderSchema = new mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer",
+        required: true,
+    },
+    //products (array of objects with productId (reference to Product) (required) and quantity (number)
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    //total price of the order
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    //status of the order (pending, shipped, delivered, cancelled)
+    status: {
+        type: String,
+        enum: ["pending", "shipped", "delivered", "cancelled"],
+        default: "pending",
+    },
+});
+
+// Create a model based on the schema
+const Order = mongoose.model("Order", orderSchema);
+
+// Export the model
+module.exports = Order;
