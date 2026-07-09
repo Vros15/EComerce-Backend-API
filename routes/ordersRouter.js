a048dc401bb49a8e5b6c9fa111866eca23f9b3be
Victor Rosario <vros15@yahoo.com>
2026-07-09 17:01:49 -0400

    chore: set up initial orders router structure

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,15 +1,18 @@
 //TODO: Implement the orders router for handling order-related API endpoints.
 
 // Import the Express router and the orders controller
+const express = require("express");
+const router = express.Router();
+const {} = require("../controllers/ordersController");
 
 // Define the routes for handling order-related API endpoints
 
-//GET all orders
+//GET all orders & Filter by status
 
 //GET a single order by ID
 
 //POST create a new order
 
-//PUT update an order by ID
+//PUT update status of an order by ID
 
 //DELETE delete an order by ID
