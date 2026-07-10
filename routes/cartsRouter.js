95d691296a4beb7e0b159e049d167746bc081079
Victor Rosario <vros15@yahoo.com>
2026-07-10 17:48:44 -0400

    feat: add route for updating cart  product quantity for Endpoint: PUT /api/carts/:customer/products

--- routes/cartsRouter.js
+++ routes/cartsRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {createOneCart, getCart,addProductToCart,removeProductFromCart, clearCart} = require("../controllers/cartsController");
+const {createOneCart, getCart,addProductToCart,removeProductFromCart, clearCart, updateQuantity} = require("../controllers/cartsController");
 
 // Route to create a new cart
 //endpoint: POST /api/carts
@@ -21,5 +21,9 @@ router.delete("/:customer/products", removeProductFromCart);
 // Route to clear a cart by customer
 //endpoint: DELETE /api/carts/:customer/clear
 router.delete("/:customer/clear", clearCart);
+
+// Route to update the quantity of a product in a cart by customer
+//endpoint: PUT /api/carts/:customer/products
+router.put("/:customer/products", updateQuantity);
 
 module.exports = router;
